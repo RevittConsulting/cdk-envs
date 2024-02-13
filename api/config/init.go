78e7f0f4bd6f7b707b1f9129f41c625e7aca2c5d
Elliot Hallam <elliot@revitt.consulting>
2024-02-13 16:00:47 +0000

    feat(docker): docker init

--- api/config/init.go
+++ api/config/init.go
@@ -5,8 +5,8 @@ import (
 )
 
 func init() {
-	viper.SetDefault("port", 80)
-	viper.SetDefault("dbfile", "boltdb.db")
+	viper.SetDefault("port", 8080)
+	viper.SetDefault("dbfile", "mdbx.dat")
 
 	viper.SetDefault("rpc.url", "https://sepolia.drpc.org")
 	viper.SetDefault("rpc.url2", "https://rpc2.sepolia.org")
